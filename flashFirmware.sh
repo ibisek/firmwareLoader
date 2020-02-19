004938b4769830d2d290320bc18fe31b67b269de
ibisek <ibisek@protonmail.com>
2020-02-19 09:43:23 +0100

    port specification as argument

--- flashFirmware.sh
+++ flashFirmware.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 3 ]; then
     echo "USAGE:"
-    echo " ./flashFirmware.sh <FILENAME> <OGNID>"
+    echo " ./flashFirmware.sh <PORT> <FILENAME> <OGNID>"
     exit 1
 fi
 
 
 export PYTHONPATH=./src/:$PYTHONPATH
-python3 ./src/ognLoader.py $1 $2
+python3 ./src/ognLoader.py $1 $2 $3
 
